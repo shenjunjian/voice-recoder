98cfb39695866443d274e8984b6d3e03f51fcf51
shenjunjian <40288193@qq.com>
2026-07-06 14:43:50 +0800

    fix: todo1

---
+++ src/VoiceRecoder/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using H.NotifyIcon;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Input;
+
+namespace VoiceRecoder;
+
+public partial class App : Application
+{
+    public TaskbarIcon? TrayIcon { get; private set; }
+
+    public Window? Window { get; set; }
+
+    public bool HandleClosedEvents { get; set; } = true;
+
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        InitializeTrayIcon();
+    }
+
+    private void InitializeTrayIcon()
+    {
+        var exitApplicationCommand = (XamlUICommand)Resources["ExitApplicationCommand"]!;
+        exitApplicationCommand.ExecuteRequested += ExitApplicationCommand_ExecuteRequested;
+
+        TrayIcon = (TaskbarIcon)Resources["TrayIcon"]!;
+        TrayIcon.ForceCreate();
+    }
+
+    private void ExitApplicationCommand_ExecuteRequested(object? _, ExecuteRequestedEventArgs args)
+    {
+        HandleClosedEvents = false;
+        TrayIcon?.Dispose();
+        Window?.Close();
+
+        // https://github.com/HavenDV/H.NotifyIcon/issues/66
+        if (Window == null)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
